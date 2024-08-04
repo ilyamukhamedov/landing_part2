b2cae7db9bbc1e223ad60d1eeb7c7894332997b5
Ilya <honeyflym@gmail.com>
2024-08-04 18:27:39 +0300

    check deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/book-2/",
+  base: process.env.NODE_ENV === "production" ? "/book_2/" : "/",
   server: {
     port: 5173, // or any other port number you're using
   },
