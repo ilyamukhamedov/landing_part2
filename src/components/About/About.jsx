1c561d3a92b005e904debb6e86f60999dea025a0
Ilya <honeyflym@gmail.com>
2024-12-25 20:58:32 +0200

    add video

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -52,6 +52,15 @@ const About = () => {
             </p>
           </div>
         </div>
+        <div class="about__video">
+          <iframe
+            src="https://vk.com/video_ext.php?oid=223005771&id=456239020&hd=1"
+            allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+            frameborder="0"
+            allowfullscreen
+            class="about__video-block"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
